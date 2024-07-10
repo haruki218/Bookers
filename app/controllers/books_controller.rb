b33706723d94be1d6d5e59ab13d197691dd357eb
EC2 Default User <ec2-user@ip-172-31-45-163.ap-northeast-1.compute.internal>
2024-07-10 10:21:00 +0000

    [Update] destroy機能作成、トップ画面start追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to root_path
+    redirect_to books_path
   end
 
   def index
@@ -27,6 +27,11 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
   
   private
   def book_params
