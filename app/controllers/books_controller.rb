25ed0ea2e36d1a6f640c3c0a8cf1f44f39a3e5b2
EC2 Default User <ec2-user@ip-172-31-45-163.ap-northeast-1.compute.internal>
2024-07-10 12:13:19 +0000

    [Update] tableタグ追加、投稿フォームと一覧の一体化

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,13 @@
 class BooksController < ApplicationController
-  def new
+  def index
     @book = Book.new
+    @books = Book.all
   end
 
   def create
     book = Book.new(book_params)
     book.save
     redirect_to books_path
-  end
-
-  def index
-    @books = Book.all
   end
 
   def show
